6366181dcd83682bf8d6855f9b9ef96025eabbac
Nirbhay Agrawal <agrawalnirbhay@yahoo.com>
2021-12-29 19:59:39 +0530

    Added Serializers

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns =[
     path('',views.getRoutes,name='getRoutes'),
+    path('notes/',views.getNotes,name="notes"),
+    path('note/<str:pk>',views.getNote,name="note"),
 ]
