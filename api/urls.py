97b3b06b724a66ab479d609d0e4f9f115ac8479b
Nirbhay Agrawal <agrawalnirbhay@yahoo.com>
2022-01-06 20:39:56 +0530

    Added Frontend

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('',views.getRoutes,name='getRoutes'),
     path('notes/',views.getNotes,name="notes"),
-    path('note/<str:pk>',views.getNote,name="note"),
+    path('note/add/',views.addNote,name="add-note"),
+    path('note/<str:pk>/update/',views.updateNote,name="update-note"),
+    path('note/<str:pk>/delete/',views.deleteNote,name="delete-note"),
+    
+    path('note/<str:pk>/',views.getNote,name="note"),
 ]
